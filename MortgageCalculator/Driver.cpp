dd59309d0200e8f46317af19473a2e24c16b7afa
Joshua Morse <joshuawmorse@gmail.com>
2017-04-24 20:10:37 -0600

    V 0.8

--- MortgageCalculator/Driver.cpp
+++ MortgageCalculator/Driver.cpp
@@ -9,6 +9,7 @@
 #include <ctime>
 #include "Functions.h"
 #include "MortCalc.h"
+#include "MortInfo.h"
 
 using namespace std;
 
@@ -16,19 +17,21 @@ using namespace std;
 int main() {
 	//Declare Variables
 	string again = "yes";
-	double MortData, MortInfo;
+	string result, filename;
 
+	MortInfo mortData;
 
 	Header();
 	do {
-		AskForLoanInfo(MortInfo, MortData);
+		AskForLoanInfo(mortData);
 
-		if (Validate(MortInfo, MortData) == false) {
-			cout << "Your input was out of the range, please re-run the program "
+		if (Validate(mortData) == false) {
+			cout << "\nYour input was out of the range, please re-run the program "
 				<< "\nwithin the appropriate ranges.";
 		}
 		else {
-			MortCalc(MortInfo, MortData);
+			WriteMort(result, filename);
+			//MortCalc(mortData);
 		}
 
 		cout << "\n Do you want to run the program again? ";
@@ -47,85 +50,81 @@ void Header() {
 
 }
 
-
-void AskForLoanInfo(double MortInfo, double &MortData) {
+void AskForLoanInfo(MortInfo &mortData) {
 
 	float interest;
 	int principal, term;
-	mortInfo info;
+
 	cout << "\n What is the ammount of the loan in whole dollars? ";
-	cin >> info.principal;
+	cin >> mortData.principal;
 
 	cout << "\n Is the term 15, 25, or 30 years? ";
-	cin >> info.term;
+	cin >> mortData.term;
 
 	cout << "\n What is the interest rate? (enter 5, for 5\%, 3.5 for 2.5\% ";
-	cin >> info.interest;
-	cout << "\n " << info.principal;
+	cin >> mortData.interest;
 
 }
 
-bool Validate(double MortInfo, double &MortData) {
-	mortInfo info;
-	if (info.principal < 500000 && info.principal > 0 && info.term == 15 || info.term == 25
-			|| info.term == 30 && info.interest > 0 && info.interest <= 10) {
+bool Validate(MortInfo &mortData) {
+	//MortInfo mortData;
+	if (mortData.principal < 500000 && mortData.principal > 0 && mortData.term == 15 || mortData.term == 25
+		|| mortData.term == 30 && mortData.interest > 0 && mortData.interest <= 10) {
 		return true;
 	}
 	else {
-		return true;
+		return false;
 	}
 }
 
-void CalcEndOfLoan(double MortInfo, double &MortData) {
-	mortInfo info;
-	tm *currentTime;
-	time_t t = time(NULL);
+void CalcEndOfLoan(MortInfo &mortData) {
 
-	int currentYear = 1900 + currentTime->tm_year;
-	int currentMonth = currentTime->tm_mon;
-	int currentDay = currentTime->tm_mday;
-	int endYear = currentYear + info.term;
-
-	cout << "\n Loan maturity date " << currentMonth << "/" << currentDay << "/" << endYear;
+	//MortInfo mortData;
+	time_t rawTime;
+	tm *OStime;
+	time(&rawTime);
 
 
+	//get the system time
+	time(&rawTime);
+	//get the local time
+	OStime = localtime(&rawTime);
+
+	//find the start of the loan
+	mortData.monStart = OStime->tm_mon  + 1;
+	mortData.dayStart = OStime->tm_mday;
+	mortData.yearStart = OStime->tm_year + 1900;
+
+	//find the end of the loan
+	mortData.monEnd = mortData.monStart;
+	mortData.dayEnd = mortData.dayStart; //come back to get the actual end date.
+	mortData.yearEnd = mortData.yearStart + mortData.term;
+
+	//define number of days in each month
+	int dayCount[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
 }
 
-
-////////////////////////////////
-//NOT FINISHED. RE-WRITE THIS MESS
-////////////////////////////////
 bool WriteMort(string result, string filename) {
-	static int counter = 0;
-	counter++;
-	stringstream ss;
-	ss << name << counter << ".txt";
-	Date = myDate;
 
-	string sDate = myDAte.GetFromattedDAte809
-	string name;
-	cout << "\n What do you want to name the file? ";
-	getline(cin,name);
 
-	// declare the output file object
+	cout << "\n What is the name of the output file? ";
+	getline(cin, filename);
+	filename = filename + ".txt";
+	//declare output file
 	ofstream outputFile;
-
-	// Open the file for writing
-	outputFile.open(name);
-
-	// Check that we've open the file
-	if (!outputFile)
-	{
-		cout << "\n The file was not written ";
+	//open file to write
+	outputFile.open(filename);
+	//check that file is open
+	if (!outputFile) {
+		cout << "\n The file was not written";
 		return false;
 	}
 
-	// Write to the file
-	outputFile << loanInfo << endl;
-
-
-	// close the file
+	//write the file by calling MortCalc()
+	outputFile << MortCalc(mortData);
+	//close the file after writing
 	outputFile.close();
 	cout << "\n The file was written. ";
 	return true;
+
 }
