ee7d7e0b7265781105d619a0ded1da98f7b75609
Joshua Morse <joshuawmorse@gmail.com>
2017-04-24 20:12:30 -0600

    Update MortCalc.h

--- MortgageCalculator/MortCalc.h
+++ MortgageCalculator/MortCalc.h
@@ -4,18 +4,29 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <cmath>
 #include "Functions.h"
+//#include "MortInfo.h"
 
 using namespace std;
 
+MortInfo mortData;
 
-string MortCalc(double MortInfo, double &MortData) {
-	mortInfo info;
-	double monthlyPayment = (info.principal * info.interest) / (12 * (1 - (pow(1 + info.interest / 12), -(info.term * 12))));
-	double totalLoan = (monthlyPayment * 12) * info.term;
-	double totalInterest = totalLoan - info.principal;
 
-	CalcEndOfLoan(MortInfo, MortData);
+string MortCalc(MortInfo &mortData) {
+	double monthlyPayment = (mortData.principal * mortData.interest) / (12 * (1 - (/*pow*/(1 + mortData.interest / 12)/*,*/ -(mortData.term * 12))));
+	double totalLoan = (monthlyPayment * 12) * mortData.term;
+	double totalInterest = totalLoan - mortData.principal;
+
+	CalcEndOfLoan(mortData);
+
+	stringstream mortInfoOutput;
+	mortInfoOutput << "\n Total Loan Amount $" << totalLoan << "\n Monthly Payment : $" << monthlyPayment
+		<< "\n Total Interest $" << totalInterest
+		<< "\n Start date: " << mortData.monStart << "/" << mortData.dayStart << "/" << mortData.yearStart
+		<< "\n Maturaty date: " << mortData.monEnd << "/" << mortData.dayEnd << "/" << mortData.yearEnd;
+
+	return mortInfoOutput.str();
 
 }
