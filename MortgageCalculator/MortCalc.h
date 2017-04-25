c6747df3266c9f6a4fa097138770fb9b14d949da
Joshua Morse <joshuawmorse@gmail.com>
2017-04-24 21:27:01 -0600

    V 0.8.5

--- MortgageCalculator/MortCalc.h
+++ MortgageCalculator/MortCalc.h
@@ -15,18 +15,22 @@ MortInfo mortData;
 
 
 string MortCalc(MortInfo &mortData) {
-	double monthlyPayment = (mortData.principal * mortData.interest) / (12 * (1 - (/*pow*/(1 + mortData.interest / 12)/*,*/ -(mortData.term * 12))));
+
+	double interest = mortData.interest / 100;
+	double monthlyPayment = (mortData.principal * interest) / (12 * (1 - (pow((1 + interest / 12), - (mortData.term * 12)))));
 	double totalLoan = (monthlyPayment * 12) * mortData.term;
 	double totalInterest = totalLoan - mortData.principal;
 
 	CalcEndOfLoan(mortData);
+	cout << "\n " << totalLoan << "\n " << monthlyPayment << "\n " << totalInterest;
 
-	stringstream mortInfoOutput;
-	mortInfoOutput << "\n Total Loan Amount $" << totalLoan << "\n Monthly Payment : $" << monthlyPayment
+	stringstream ss;
+	ss << "\n Total Loan Amount $" << totalLoan << "\n Monthly Payment : $" << monthlyPayment
 		<< "\n Total Interest $" << totalInterest
 		<< "\n Start date: " << mortData.monStart << "/" << mortData.dayStart << "/" << mortData.yearStart
 		<< "\n Maturaty date: " << mortData.monEnd << "/" << mortData.dayEnd << "/" << mortData.yearEnd;
 
-	return mortInfoOutput.str();
+
+	return ss.str();
 
 }
